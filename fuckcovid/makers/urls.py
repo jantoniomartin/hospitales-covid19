45057ea9a78982d414cc9e35a7b36cea3d2077ee
José Antonio Martín Prieto <jantonio.martin@gmail.com>
2020-03-20 10:28:18 +0100

    makers can be created and updated

--- fuckcovid/makers/urls.py
+++ fuckcovid/makers/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.RegionList.as_view(), name='region-list'),
     path('regions/<int:pk>/', views.RegionDetail.as_view(), name='region-detail'),
     path('maker/<int:pk>/', views.MakerDetail.as_view(), name='maker-detail'),
+    path('maker/create/', views.MakerCreate.as_view(), name='maker-create'),
+    path('maker/<int:pk>/update/', views.MakerUpdate.as_view(), name='maker-update'),
 ]
--- fuckcovid/makers/views.py
+++ fuckcovid/makers/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from fuckcovid.hospitals.models import Region
 from fuckcovid.makers.models import Maker
@@ -32,3 +34,16 @@ class MakerDetail(DetailView):
         context['productions'] = self.object.production_set.order_by('resource__name')
         return context
 
+
+class MakerCreate(LoginRequiredMixin, CreateView):
+    model = Maker
+    fields = ['name', 'city', 'phone', 'address', 'region', 'comment']
+
+    def form_valid(self, form):
+        form.instance.editor = self.request.user
+        return super().form_valid(form)
+
+
+class MakerUpdate(LoginRequiredMixin, UpdateView):
+    model = Maker
+    fields = ['name', 'city', 'phone', 'address', 'region', 'comment']
